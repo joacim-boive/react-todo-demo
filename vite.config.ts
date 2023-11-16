783da0bc79ebc3b626d3484ccdcf042b9faa3043
Joacim Boive <joacim.boive@gmail.com>
2023-11-16 08:34:41 +0100

    Fix vercel deploy (#5)
    * casing problem
    
    * fix casing mismatch
    
    * Casing...
    
    * Update vite build to publish to public folder
    
    * Setting up node on Vercel
    
    * We're not using Vercel - "can't" host socket server.
    
    ---------
    
    Co-authored-by: Joacim Boive <jobo15@handelsbanken.se>

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+    outDir: "public",
+  },
   plugins: [react()],
   resolve: {
     alias: {
